2609968405523fbd3aa2105db1a326f3eb29fde6
ac sexton <hello@acsexton.com>
2020-06-10 14:47:19 -0400

    refactor to use api view wrappers

--- drfPasteBin/drfPasteBin/urls.py
+++ drfPasteBin/drfPasteBin/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # TODO: Does this need to leave here?
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
 ]
