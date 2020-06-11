a51bc1e39b3e5b02ad5ebec11af63d50a97d3bad
ac sexton <hello@acsexton.com>
2020-06-11 10:14:29 -0400

    add pagination to settings

--- drfPasteBin/drfPasteBin/settings.py
+++ drfPasteBin/drfPasteBin/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Application definition
 
