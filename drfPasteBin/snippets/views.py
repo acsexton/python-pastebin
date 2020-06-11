030a72380630b8d8f65018a45116cbb2a616f917
ac sexton <hello@acsexton.com>
2020-06-11 10:34:12 -0400

    implement viewsets to generalize user and snippet list-detail logic

--- drfPasteBin/snippets/views.py
+++ drfPasteBin/snippets/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import renderers
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from snippets.models import Snippet
@@ -11,50 +12,32 @@ from snippets.serializers import UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
     """
-    List all code snippets, or create a new snippet.
-    """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    Model viewset automatically provides 'list', 'create', 'retrieve', 'update', and 'destroy'
+    actions
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve, update, or delete a code snippet.
+    We add a 'highlight' action here
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-
-class SnippetHighlight(generics.GenericAPIView):
-    """
-    Establish an endpoint for a highlighted endpoint
-    """
-    queryset = Snippet.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        """
+        Will be used to add custom endpoints that don't fit standard create/update/delete style
+        """
         snippet = self.get_object()
         return Response(snippet.highlighted)
 
-
-class UserList(generics.ListAPIView):
-    """
-    List all users
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    Retrieve user data
+    ReadOnly viewset automatically provides 'list' and 'detail' actions
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
