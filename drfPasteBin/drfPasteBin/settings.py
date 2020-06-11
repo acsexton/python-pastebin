bc98842dddf408369076a84cb1604237dd74eaa4
ac sexton <hello@acsexton.com>
2020-06-11 11:18:46 -0400

    implement dotenv and .env file (ignored) for secret key

--- drfPasteBin/drfPasteBin/settings.py
+++ drfPasteBin/drfPasteBin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kg#d!(7k)abj7ar$un)*$dq0mziq)^d)k@^%qpvy35#f&q-d@u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
