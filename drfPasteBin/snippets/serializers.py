d4dbb690ba675492f94df47b606fd06f08c67713
ac sexton <hello@acsexton.com>
2020-06-11 09:48:08 -0400

    handle initial auth functionality, allow login for creating snippets

--- drfPasteBin/snippets/serializers.py
+++ drfPasteBin/snippets/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
     def create(self, validated_data):
         """
@@ -22,5 +23,14 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.linenos = validated_data.get('linenos', instance.linenos)
         instance.language = validated_data.get('language', instance.language)
         instance.style = validated_data.get('style', instance.style)
+        instance.owner = serializers.ReadOnlyField(source='owner.username')
         instance.save()
         return instance
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
--- drfPasteBin/snippets/views.py
+++ drfPasteBin/snippets/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from snippets.serializers import UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -9,6 +12,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -17,3 +24,20 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    """
+    List all users
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    """
+    Retrieve user data
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
